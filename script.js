a99ec91e1fbeb941de977aa2c381e133b26dfb76
MUNAZIR <60435223+Munazirraza@users.noreply.github.com>
2023-08-22 23:28:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,39 @@
-
-
-  //your code here
-	function convertToRoman(num) {
-  if (num <= 0 || num > 100000) {
-    return "Number out of range";
-  }
-
-  const romanSymbols = {
-    M: 1000,
-    CM: 900,
-    D: 500,
-    CD: 400,
-    C: 100,
-    XC: 90,
-    L: 50,
-    XL: 40,
-    X: 10,
-    IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1,
+function convertToRoman(num) {
+  const obj = {
+    0: ['M', 1000],
+    1: ['D', 500],
+    2: ['C', 100],
+    3: ['L', 50],
+    4: ['X', 10],
+    5: ['V', 5],
+    6: ['I', 1]
   };
 
-  let romanNumeral = "";
+  let romanNumeral = '';
 
-  for (let symbol in romanSymbols) {
-    while (num >= romanSymbols[symbol]) {
+  for (let i = 0; i < Object.keys(obj).length; i++) {
+    const symbol = obj[i][0];
+    const value = obj[i][1];
+
+    // Repeat the symbol while subtracting its value from the number
+    while (num >= value) {
       romanNumeral += symbol;
-      num -= romanSymbols[symbol];
+      num -= value;
+    }
+
+    // Handle subtractive notation by checking if the current symbol can be combined with the next symbol
+    if (i % 2 === 0 && i < Object.keys(obj).length - 1) {
+      const nextSymbol = obj[i + 2][0];
+      const nextValue = obj[i + 2][1];
+
+      if (num >= value - nextValue) {
+        romanNumeral += nextSymbol + symbol;
+        num -= value - nextValue;
+      }
     }
   }
 
   return romanNumeral;
 }
 
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
+// console.log(convertToRoman(36)); // Output: XXXVI
